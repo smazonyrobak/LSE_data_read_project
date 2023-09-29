e6fb9f65f0e57aa82f88f40eb90ba3814f97118c
Szymon_lic <szymon.lic.22@ucl.ac.uk>
2023-09-29 15:03:45 +0100

    finished creating NaN blocks identifier in missing_values_interpolater.py, made two versions of it, applied to head in main.py.

--- main.py
+++ main.py
@@ -1,28 +1,35 @@
-import h5py
-
 from data_loading import load_sleap_file
 from datastructure_read import datastructure_read
+from missing_values_interpolater import point_finder, point_finder_simple
+from micetracker import micetrack
 
-
-fpath="./first_sample_analysis.h5"
+analysisfile="./first_sample_analysis.h5"
 dataset, dataset_keys, tracked_bodypart_names = load_sleap_file()
 
 while True:
-    what_to_do = str(input("\n Do you want to: \n a) visualise the data structure - write s \n"
-                           " b) visualize mice tracking - write t \n"
-                           "c) interpolate the data - write i \n"
-                           "d) exit the program - write e)"))
+    what_to_do = str(input("\n Do you want to: \n a) visualise the data structure - write a \n"
+                           " b) visualize mice tracking - write b \n"
+                           "c) see data about missing values for head - write c \n"
+                           "d) exit the program - write d)"))
 
-    #from here on, depending on the desired actions, other executables are called
-    if what_to_do == "s":
-        datastructure_read(fpath, dataset, dataset_keys, tracked_bodypart_names)
-    elif what_to_do == "t":
-        with open("micetracker.py") as mt:
-            exec(mt.read())
-    elif what_to_do == "i":
-        with open ("missing_values_interpolater.py") as mvi:
-            exec(mvi.read())
-    elif what_to_do == "e":
+    #from here on, depending on the desired actions, other functions from other files are called
+    if what_to_do == "a":
+        datastructure_read(analysisfile, dataset, dataset_keys, tracked_bodypart_names) #prints data structure info
+    elif what_to_do == "b": #prints tracking of mice
+         micetrack()
+    elif what_to_do == "c": #prints head missing values info
+        print("\033[92mnumber of individual periods with no values assigned for head (using while loop):\033[0m\n", len(point_finder(dataset)))
+        print("\033[92m2D array of respectively first and last value between NaN blocks in head body part (using while loop):\033[0m\n", point_finder(dataset))
+        pairs = point_finder(dataset)
+        number = 0
+        for x in pairs:
+            number += ((x[1]-1) - x[0]) #I am subtracting 1 from x[1] because e.g. [T, F, F ,F ,T] has 3 F's but would otherwise be counted as 4 F's
+        print("\033[92mcount of numbers in the interval (using while loop):\033[0m\n", number)
+
+        print("\033[92mtransition indices for video frames using np.isnan (changes of T -> F or F -> T):\033[0m\n", point_finder_simple(dataset))
+        print("\033[92mcount of transition points (changes of T -> F or F -> T):\n\033[0m", len(point_finder_simple(dataset)))
+
+    elif what_to_do == "d": #exits program
         exit()
     else:
         print("Invalid option, please try again")
--- missing_values_interpolater.py
+++ missing_values_interpolater.py
@@ -1,22 +1,27 @@
-def linear_interpolation(array):
-    missing_values = np.where(~np.isnan(array))[0]
-    if len(missing_values) < 2:
-        return array
-    else:
-        last_valid_value = None
-        for value in array:
-            if not np.isnan(value):
-                if last_valid_value is not None:
-                    print(last_valid_value, value)
-                last_valid_value = value
-            elif last_valid_value is not None:
-                continue
+import numpy as np
 
-linear_interpolation(dataset[:, 0, 0, 0])
+def point_finder(array):
+    pairs = []
+    i = 1
+    while i < array.shape[0]:
+        value_1 = array[i, 0, 0, 0]
+        value_1_before = array[i-1, 0, 0, 0]
+        if np.isnan(value_1) and not np.isnan(value_1_before):
+            for ii in range(i+1, array.shape[0]):
+                value_2 = array[ii, 0, 0, 0]
+                value_2_before = array[ii-1, 0, 0, 0]
+                if not np.isnan(value_2) and np.isnan(value_2_before):
+                    pairs.append ([i-1, ii])
+                    i = ii + 1
+                    break
+        else:
+            i += 1
+    return(pairs)
 
 
-
-
-
-
+def point_finder_simple(array):
+    Nan_list = np.isnan(array[:, 0, 0, 0])
+    Nan_list_change = np.logical_xor(Nan_list != np.roll(Nan_list, 1), Nan_list != np.roll(Nan_list, -1))
+    indices_where_true = np.where(Nan_list_change)[0]  # This will give you the indices where Nan_list_change is True
+    return list(indices_where_true)
 
